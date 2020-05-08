00746124455a271959de4cfc80c61456e7e4e21f
hhjyhxw <512162086@qq.com>
2020-05-08 18:00:15 +0800

    update

--- src/main/java/com/icloud/modules/scanactivity/dao/ScanactivityUserScanrecordMapper.java
+++ src/main/java/com/icloud/modules/scanactivity/dao/ScanactivityUserScanrecordMapper.java
@@ -2,14 +2,16 @@ package com.icloud.modules.scanactivity.dao;
 
 import com.icloud.modules.scanactivity.entity.ScanactivityUserScanrecord;
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import java.util.List;
+import java.util.Map;
 
 /**
- * 
- * 
+ *
  * @author zdh
  * @email yyyyyy@cm.com
- * @date 2020-04-01 15:25:21
+ * @date 2020-05-08 17:24:59
  */
 public interface ScanactivityUserScanrecordMapper extends BaseMapper<ScanactivityUserScanrecord> {
-	
+
+    List<ScanactivityUserScanrecord> queryMixList(Map<String,Object> map);
 }
